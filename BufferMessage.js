b4f8025920788e56c557eaed8fbdb8e64bf23efe
Noah Peterson <NoahBPeterson1997@gmail.com>
2022-08-29 18:57:28 -0500

    Adding new read/write functions to BufferMessage to support more data types and making some changes to remove bugs.

--- BufferMessage.js
+++ BufferMessage.js
@@ -2,14 +2,25 @@ const { Buffer } = require('buffer');
 
 class BufferMessage
 {
-    constructor()
+    constructor(buffer)
     {
-        this.messageBuffer = new Buffer.from([0x86]);
+        if (buffer == null)
+        {
+            this.messageBuffer = new Buffer.from([0x86]);
+            this.offset = 1;
+        }
+        else
+        {
+            this.messageBuffer = buffer;
+            if (this.messageBuffer[0] == 134)
+                this.offset = 1;    
+        }
     }
 
     Clear()
     {
-        this.messageBuffer = new Buffer.from([0x86])
+        this.messageBuffer = new Buffer.from([0x86]);
+        this.offset = 1;
         return this;
     }
 
@@ -24,21 +35,122 @@ class BufferMessage
         return this;
     }
 
+    ReadByte(offset = -1)
+    {
+        var byte = 0;
+        if (offset == -1)
+        {
+            console.assert(this.offset <= (this.messageBuffer.byteLength - 1), "Tried to read 1 byte from message buffer where there isn't 1 byte.")
+            byte = this.messageBuffer.readInt8(this.offset);
+            this.offset += 1;
+        }
+        else
+        {
+            console.assert(offset <= (this.messageBuffer.byteLength - 1), "Tried to read 1 byte from message buffer where there isn't 1 byte.")
+            byte = this.messageBuffer.readInt8(offset);
+        }
+        return byte;
+    }
+
+    ReadShort(offset = -1)
+    {
+        var short = 0;
+        if (offset == -1)
+        {
+            console.assert(this.offset <= (this.messageBuffer.byteLength - 2), "Tried to read 2 bytes from message buffer where there isn't 2 bytes.")
+            short = this.messageBuffer.readInt16BE(this.offset);
+            this.offset += 2;
+        }
+        else
+        {
+            console.assert(offset <= (this.messageBuffer.byteLength - 2), "Tried to read 2 bytes from message buffer where there isn't 2 bytes.")
+            short = this.messageBuffer.readInt16BE(offset);
+        }
+        return short;
+    }
+
+    ReadInt(offset = -1)
+    {
+        var int = 0;
+        if (offset == -1)
+        {
+            console.assert(this.offset <= (this.messageBuffer.byteLength - 4), "Tried to read 4 bytes from message buffer where there isn't 4 bytes.")
+            int = this.messageBuffer.readInt32BE(this.offset);
+            this.offset += 4;
+        }
+        else
+        {
+            console.assert(offset <= (this.messageBuffer.byteLength - 4), "Tried to read 4 bytes from message buffer where there isn't 4 bytes.")
+            int = this.messageBuffer.readInt32BE(offset+3);
+        }
+        return int;
+    }
+
+    ReadLong(offset = -1)
+    {
+        var long = 0;
+        if (offset == -1)
+        {
+            console.assert(this.offset <= (this.messageBuffer.byteLength - 8), "Tried to read 8 bytes from message buffer where there isn't 8 bytes.")
+            this.ReadInt(); // Throwaway. We don't really handle numbers larger than 2^31, but we have to be able to send 8 byte integers.
+            long = this.ReadInt();
+        }
+        else
+        {
+            console.assert(offset <= (this.messageBuffer.byteLength - 8), "Tried to read 8 bytes from message buffer where there isn't 8 bytes.")
+            long = this.ReadInt(offset+4);
+        }
+        return long;
+    }
+
+    ReadString(offset = -1)
+    {
+        var string = "";
+        if (offset == -1)
+        {
+            var stringLength = this.ReadShort();
+            string = this.messageBuffer.toString('utf8', this.offset, this.offset + stringLength);
+            this.offset += stringLength;
+        }
+        else
+        {
+            var stringLength = this.ReadShort(offset);
+            string = this.messageBuffer.toString('utf8', offset, offset+1+stringLength);
+        }
+        return string;
+    }
+
+    WriteByte(byte = 0)
+    {
+        console.assert(byte < 128 && byte >= -128); // [-2^7, (2^7)-1]
+        var buffer = Buffer.alloc(1);
+        buffer.writeIntBE(byte, 0, 1);
+        this.messageBuffer = Buffer.concat([this.messageBuffer, buffer]);
+        return this;
+    }
+
     WriteShort(short = 0)
     {
         console.assert(short < 32768 && short >= -32768); // [-2^15, (2^15)-1]
-        let byteArray = [];
-        if (short < 256)
-        {
-            byteArray = [0, short];
-        } else
-        {
-            let upperByte = short >> 8;
-            let lowerByte = short % 256;
-            byteArray = [upperByte, lowerByte];
-        }
+        var buffer = Buffer.alloc(2);
+        buffer.writeIntBE(short, 0, 2);
+        this.messageBuffer = Buffer.concat([this.messageBuffer, buffer]);
+        return this;
+    }
 
-        this.messageBuffer = Buffer.concat([this.messageBuffer, Buffer.from(byteArray)]);
+    WriteInt(int = 0)
+    {
+        console.assert(int < 2147483648 && int >= -2147483648); // [-2^31, (2^31)-1]
+        var buffer = Buffer.alloc(4);
+        buffer.writeIntBE(int, 0, 4);
+        this.messageBuffer = Buffer.concat([this.messageBuffer, buffer]);
+        return this;
+    }
+
+    WriteLong(long = 0)
+    {
+        console.assert(long < 2147483648 && long >= -2147483648); // [-2^31, (2^31)-1]
+        this.WriteInt(0).WriteInt(long);
         return this;
     }
 
@@ -46,10 +158,10 @@ class BufferMessage
     {
         if (string.length == 0 || string == null) throw Error("Cannot write an empty or null string.");
         this.WriteShort(string.length);
-        this.messageBuffer = Buffer.concat([this.messageBuffer, Buffer.from(string)]);
+        var stringBuffer = Buffer.from(string);
+        this.messageBuffer = Buffer.concat([this.messageBuffer, stringBuffer]);
         return this;
     }
 }
-
 
 module.exports = { BufferMessage }
--- Tests.js
+++ Tests.js
@@ -1,15 +1,109 @@
 const {BufferMessage} = require("./BufferMessage.js");
 const {PacketType} = require("./PacketType.js");
 
+let packetErrorMessage = "Packet bytes did not write correctly to the buffer.";
+let byteErrorMessage = "BufferMessage did not read/write a byte correctly! ";
+let shortErrorMessage = "BufferMessage did not read/write a short-integer correctly! ";
+let intErrorMessage = "BufferMessage did not read/write an integer correctly! ";
+let longErrorMessage = "BufferMessage did not read/write a long correctly! ";
+let stringErrorMessage = "BufferMessage did not read/write a string correctly! ";
+
 let testBufferMessage = new BufferMessage();
 console.assert(
     Buffer.from([134, 0, 142]).compare(testBufferMessage.Clear().WriteShort(PacketType.AcceptedFactionInvite.id).GetMessage()) == 0,
-    "Packet bytes did not write correctly to the buffer.");
+    packetErrorMessage+JSON.stringify(testBufferMessage));
 console.assert(
     Buffer.from([134, 0, 143]).compare(testBufferMessage.Clear().WriteShort(PacketType.AcceptedFactionInvite.id).GetMessage()) != 0,
-    "Packet bytes did not write correctly to the buffer.");
+    packetErrorMessage+JSON.stringify(testBufferMessage));
 console.assert(
     Buffer.from([134, 125, 2]).compare(testBufferMessage.Clear().WriteShort(PacketType.RadioPostSilenceEvent.id).GetMessage()) == 0,
-    "Packet bytes did not write correctly to the buffer.");
+    packetErrorMessage+JSON.stringify(testBufferMessage));
+
+/// Test a mix of reading/writing Short, Int, String, and Long.
+testBufferMessage.Clear();
+var string = "testMessageLength";
+var string2 = "testingMessagingLengthening";
+var string3 = "tML";
+testBufferMessage
+    .WriteShort(42)
+    .WriteShort(10752)
+    .WriteInt(100_000)
+    .WriteString(string)
+    .WriteString(string2)
+    .WriteString(string3)
+    .WriteLong(2_147_483_638);
+
+var readShort = testBufferMessage.ReadShort();
+var readShort2 = testBufferMessage.ReadShort();
+var readInt = testBufferMessage.ReadInt();
+var readString = testBufferMessage.ReadString();
+var readString2 = testBufferMessage.ReadString();
+var readString3 = testBufferMessage.ReadString();
+var readLong = testBufferMessage.ReadLong();
+console.assert(readShort == 42, shortErrorMessage + JSON.stringify(readShort));
+console.assert(readShort2 == 10752, shortErrorMessage + JSON.stringify(readShort2));
+console.assert(readInt == 100_000, intErrorMessage + JSON.stringify(readInt));
+console.assert(readString.includes(string), stringErrorMessage+ JSON.stringify(readString));
+console.assert(readString2.includes(string2), stringErrorMessage+ JSON.stringify(readString2));
+console.assert(readString3.includes(string3), stringErrorMessage+ JSON.stringify(readString3));
+console.assert(readLong == 2_147_483_638, longErrorMessage + readLong);
+
+/// Test reading/writing 8-byte longs.
+var testBufferMessageLong = new BufferMessage();
+testBufferMessageLong
+    .WriteLong(1_000_000)
+    .WriteLong(500_000)
+    .WriteLong(4);
+var longs = [testBufferMessageLong.ReadLong(), testBufferMessageLong.ReadLong(), testBufferMessageLong.ReadLong()];
+console.assert(longs[0] == 1_000_000, longErrorMessage + JSON.stringify(longs[0]));
+console.assert(longs[1] == 500_000, longErrorMessage + JSON.stringify(longs[1]));
+console.assert(longs[2] == 4, longErrorMessage + JSON.stringify(longs[2]));
+
+/// Test reading/writing 4-byte integers.
+var testBufferMessageInt = new BufferMessage();
+testBufferMessageInt
+    .WriteInt(1_000_000)
+    .WriteInt(500_000)
+    .WriteInt(4);
+var integers = [testBufferMessageInt.ReadInt(), testBufferMessageInt.ReadInt(), testBufferMessageInt.ReadInt()];
+console.assert(integers[0] == 1_000_000, intErrorMessage + JSON.stringify(integers[0]));
+console.assert(integers[1] == 500_000, intErrorMessage + JSON.stringify(integers[1]));
+console.assert(integers[2] == 4, intErrorMessage + JSON.stringify(integers[2]));
+
+/// Test reading/writing 2-byte shorts.
+var testBufferMessageShort = new BufferMessage();
+testBufferMessageShort
+    .WriteShort(30_000)
+    .WriteShort(256)
+    .WriteShort(30);
+var shorts = [testBufferMessageShort.ReadShort(), testBufferMessageShort.ReadShort(), testBufferMessageShort.ReadShort()];
+console.assert(shorts[0] == 30_000, shortErrorMessage + JSON.stringify(shorts[0]));
+console.assert(shorts[1] == 256, shortErrorMessage + JSON.stringify(shorts[1]));
+console.assert(shorts[2] == 30, shortErrorMessage + JSON.stringify(shorts[2]));
+
+/// Test reading/writing 1-byte numbers.
+var testBufferMessageByte = new BufferMessage();
+testBufferMessageByte
+    .WriteByte(120)
+    .WriteByte(-120)
+    .WriteByte(10);
+var bytes = [testBufferMessageByte.ReadByte(), testBufferMessageByte.ReadByte(), testBufferMessageByte.ReadByte()];
+console.assert(bytes[0] == 120, byteErrorMessage + JSON.stringify(bytes[0]));
+console.assert(bytes[1] == -120, byteErrorMessage + JSON.stringify(bytes[1]));
+console.assert(bytes[2] == 10, byteErrorMessage + JSON.stringify(bytes[2]));
+
+/// Test reading/writing strings.
+var testBufferMessageString = new BufferMessage();
+testBufferMessageString
+    .WriteString("lengthOne")
+    .WriteString("longerLengthString")
+    .WriteString("longestStringOfThemAll");
+var strings = [testBufferMessageString.ReadString(), testBufferMessageString.ReadString(), testBufferMessageString.ReadString()];
+console.assert(strings[0] == "lengthOne", stringErrorMessage + JSON.stringify(strings[0]));
+console.assert(strings[1] == "longerLengthString", stringErrorMessage + JSON.stringify(strings[1]));
+console.assert(strings[2] == "longestStringOfThemAll", stringErrorMessage + JSON.stringify(strings[2]));
+
+
+// console.log(JSON.stringify(testBufferMessage));
 
 console.log("Finished running tests.");
